e8581c2b78f44d6fe1338c49cf8833827f169fd6
Gaussian-12 <malisawopatrick@gmail.com>
2026-05-26 21:26:37 +0200

    Trigger redeploy to fix 503 error

--- app.py
+++ app.py
@@ -3325,4 +3325,4 @@ if __name__ == '__main__':
     print("║  Admin:  admin@school.mw  /  Admin@2025             ║")
     print("║  URL:    http://localhost:5000                       ║")
     print("╚══════════════════════════════════════════════════════╝")
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)# redeploy trigger
